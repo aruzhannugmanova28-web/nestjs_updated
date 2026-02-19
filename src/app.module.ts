69aef4af062333ec9f2d7c9aa7e28bd6144eec26
Aruzhan Nugmanova <aruzhan.nugmanova@mac.milton.edu>
2026-02-19 08:41:20 -0500

    started mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose';
+
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from 'product/products.module';
