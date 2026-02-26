b9de6cde7abb6de7468b7b40f00689547936a2a9
Aruzhan Nugmanova <aruzhan.nugmanova@mac.milton.edu>
2026-02-26 12:03:37 -0500

    moongose resolved

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { AppService } from './app.service';
 import { ProductsModule } from 'product/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://user_0:user_0@cluster0.6e19qrz.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
