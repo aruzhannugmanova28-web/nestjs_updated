f222399f6f3bd6c0ddd4cef24995c74595eb1124
Aruzhan Nugmanova <aruzhan.nugmanova@mac.milton.edu>
2026-03-04 23:52:23 -0500

    async everything...

--- product/product.controller.ts
+++ product/product.controller.ts
@@ -6,12 +6,12 @@ export class ProductsController{
     constructor(private readonly productsService: ProductsService){}
 
     @Post()
-    addProduct(
+    async addProduct(
         @Body('title') prodTitle: string, 
         @Body('description') prodDesc: string, 
         @Body('price') prodPrice: number,
-    ): any{
-        const generatedId = this.productsService.insertProduct(
+    ) {
+        const generatedId = await this.productsService.insertProduct(
             prodTitle,
             prodDesc, 
             prodPrice,
@@ -20,8 +20,9 @@ export class ProductsController{
     }
     
     @Get()
-    getAllProducts(){
-        return this.productsService.getProducts();
+    async getAllProducts(){
+        const products = await this.productsService.getProducts();
+        return products;
     }
 
     @Get(':id')
--- product/products.service.ts
+++ product/products.service.ts
@@ -15,43 +15,47 @@ export class ProductsService {
         const newProduct = new this.productModel({title: title, description: desc, price: price });
         const result = await newProduct.save();
         console.log(result);
-        //return prodId;
+        return result.id as string;
     }
-    getProducts(){
-        return [...this.products];
+    async getProducts(){
+        const products = await this.productModel.find().exec();
+        return products.map((prod) => ({id: prod.id, title: prod.title, description: prod.description, price: prod.price}));
     }
 
-    getSingleProduct(productId: string) {
-        const product = this.findProduct(productId)[0];
-        return {...product};
+    async getSingleProduct(productId: string) {
+        const product = await this.findProduct(productId);
+        return product;
     }
 
     updateProduct(productId: string, title:string, desc: string, price: number) {
-        const [product, index] = this.findProduct(productId);
-        const updatedProduct = {...product };
-        if (title) {
-            updatedProduct.title= title;
-        }
-        if (desc) {
-            updatedProduct.description= desc;
-        }
-        if (price) {
-            updatedProduct.price= price;
-        }
-        this.products[index]= updatedProduct as Product;
+        //const [product, index] = this.findProduct(productId);
+        //const updatedProduct = {...product };
+        //if (title) {
+       //    updatedProduct.title= title;
+        //}
+        //if (desc) {
+       //     updatedProduct.description= desc;
+        //}
+        //if (price) {
+       //     updatedProduct.price= price;
+       // }
+       // this.products[index]= updatedProduct as Product;
     }
     deleteProduct(prodId: string){
         const index = this.findProduct(prodId)[1];
         this.products.splice(index, 1);
     }
 
-    private findProduct(id: string): [Product, number]{
-        const productIndex = this.products.findIndex(prod=> prod.id === id);
-        const product = this.products[productIndex];
+    private async findProduct(id: string): Promise<Product> {
+        const product = await this.productModel.findById(id);
         if (!product) {
             throw new NotFoundException('Could not find product');
         }
-        return [product, productIndex];
+        return {
+            id: product.id, 
+            title: product.title, 
+            description: product.description, 
+            price: product.price
+        };
     }
-
 }
