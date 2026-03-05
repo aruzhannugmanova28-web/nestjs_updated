c7b9e1f21e269c14a002611e41690249c53f0186
Aruzhan Nugmanova <aruzhan.nugmanova@mac.milton.edu>
2026-03-05 08:37:33 -0500

    almost finished

--- product/products.service.ts
+++ product/products.service.ts
@@ -24,38 +24,43 @@ export class ProductsService {
 
     async getSingleProduct(productId: string) {
         const product = await this.findProduct(productId);
-        return product;
+        return {id: product.id, title: product.title, description: product.description, price: product.price};
     }
 
-    updateProduct(productId: string, title:string, desc: string, price: number) {
-        //const [product, index] = this.findProduct(productId);
-        //const updatedProduct = {...product };
-        //if (title) {
-       //    updatedProduct.title= title;
-        //}
-        //if (desc) {
-       //     updatedProduct.description= desc;
-        //}
-        //if (price) {
-       //     updatedProduct.price= price;
-       // }
-       // this.products[index]= updatedProduct as Product;
+  async updateProduct(
+    productId: string,
+    title: string,
+    desc: string,
+    price: number,
+  ) {
+    const updatedProduct = await this.findProduct(productId);
+    if (title) {
+      updatedProduct.title = title;
     }
+    if (desc) {
+      updatedProduct.description = desc;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    updatedProduct.save();
+  }
+
     deleteProduct(prodId: string){
         const index = this.findProduct(prodId)[1];
         this.products.splice(index, 1);
     }
 
     private async findProduct(id: string): Promise<Product> {
-        const product = await this.productModel.findById(id);
+        let product;
+        try{
+            product = await this.productModel.findById(id);
+        } catch(error){
+            throw new NotFoundException('Could not find product');
+        }
         if (!product) {
             throw new NotFoundException('Could not find product');
         }
-        return {
-            id: product.id, 
-            title: product.title, 
-            description: product.description, 
-            price: product.price
-        };
+        return product;
     }
 }
